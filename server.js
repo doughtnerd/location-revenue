9c58c3fbfb2e82c3e1d47057ac742e55e96f5705
Chris Carlson <doughtnerd@gmail.com>
2016-08-18 22:46:18 +0000

    Added error page

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ global.appRoot = path.resolve(__dirname);
 
 var routes = require(__dirname + "/server/routes/routes.js");
 
+//Set view engine
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/client/');
+app.set('view options', {
+    layout: false
+});
+
 app.use(express.static(__dirname + '/client/'));
 
 routes(app);
 
 app.listen(process.env.PORT || 8080, function(){
-    //console.log(process.env.NODE_PATH);
-    //console.log(require("fs").existsSync("./jar/runner.jar")?"its there":"its not there");
+    console.log('Server is listening on port: ' + process.env.PORT || 8080);
 });
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,8 @@ var upload = require(global.appRoot +"/server/config/multer-config.js");
 var exec = require("child_process").exec;
 
 module.exports = function(app){
+    var settings = {};
+    
     app.get('/', function(req, res){
         res.sendFile(global.appRoot + "/client/index.html");
     });
@@ -12,20 +14,23 @@ module.exports = function(app){
         res.download(global.appRoot + '/server/jar/Location Revenue Per Pound.jar', 'Location Revenue Per Pound.jar', function(err){
             if(err){
                 if(!res.headersSent){
-                    res.send('Error occurred while downloading file');
+                    settings.msg='Error occurred while downloading file';
+                    res.render('errorpage', settings);
                 }
             } 
         });
     });
     
     app.post('/run', upload.array('reqFiles', 2) , function(req, res){
+        
         var rev = req.files[0];
         var nat = req.files[1];
         if(rev==undefined || nat==undefined){
             rev = rev==undefined ? undefined : rev.path;
             nat = nat==undefined ? undefined : nat.path;
             common.deleteFiles(undefined, rev, nat);
-            res.send('One or more files were invalid. Ensure that only xlsx or xls files are being used then try again.');
+            settings.msg='One or more files were invalid. Ensure that only xlsx or xls files are being used then try again.';
+            res.render('errorpage', settings);
         } else {
             var hash = common.createRandomHash(); 
             var resultFileName = hash + '.xlsx';
@@ -35,12 +40,14 @@ module.exports = function(app){
                     rev = rev==undefined ? undefined : rev.path;
                     nat = nat==undefined ? undefined : nat.path;
                     common.deleteFiles(undefined, rev, nat);
-                    res.send('There was an error processing the report. Please ensure the correct source files are being used then try again.');
+                    settings.msg='There was an error processing the report. Please ensure the correct source files are being used then try again.';
+                    res.render('errorpage', settings);
                 } else {
                     res.download(resultFileLocation, resultFileName, function(err){
                         common.deleteFiles(resultFileLocation, rev.path, nat.path);
                         if(err){
-                            res.send('Application was unable to process your report for downloading.');
+                            settings.msg = 'Application was unable to process your report for downloading.';
+                            res.render('errorpage', settings);
                         } 
                     });
                 }
